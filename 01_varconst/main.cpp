e15f9585ce53b4f0d76d05b94abc8e91475ce356
MRINMOY MONDAL <mm2922894@gmail.com>
2024-09-07 01:25:04 +0530

    end 01_varconat

---
+++ 01_varconst/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  // not resignable
+  const int uid = 123456;
+
+  int score;
+
+  score = 10;
+
+  cout << "welcome to cwcpp" << endl;
+
+  return 0;
+}
