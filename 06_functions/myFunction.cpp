7fae17a796edcd2ca6a4f632a973c381c28bb8d9
MRINMOY MONDAL <mm2922894@gmail.com>
2024-09-29 01:16:23 +0530

    functions begin

---
+++ 06_functions/myFunction.cpp
@@ -0,0 +1,20 @@
+// returnType functionName(parameters){
+// functiknBody
+// }
+
+#include <iostream>
+
+int checkTempareture(int tempareture) { return tempareture; };
+
+// decrayion of function
+void serveChai(int cups) {}
+
+int main() {
+
+  int temp = checkTempareture(44);
+  std::cout << temp;
+
+  serveChai(4);
+
+  return 0;
+}
